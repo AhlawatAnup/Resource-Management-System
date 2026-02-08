70e8b1669258b203a5cde81dbe01c50745d853d0
Ankit Kumar <rajeraankit4@gmail.com>
2026-02-08 22:19:43 +0530

    added pre hook findOneAndDelete, findByIdAndDelete on teacher schema, which deletes all the students under that teacher when the teacher's account is deleted

--- server/main/database/teacherModel.js
+++ server/main/database/teacherModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Student = require("./studentModel");
 
 const teacherSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
@@ -15,6 +16,25 @@ const teacherSchema = new mongoose.Schema({
   ],
   is_verified: { type: Boolean, default: false },
   verification_completed: { type: Boolean, default: false }, 
+});
+
+// Cascade delete: when teacher is deleted, delete all associated students
+teacherSchema.pre("findOneAndDelete", async function(next) {
+  const teacher = await this.model.findOne(this.getFilter());
+  if (teacher && teacher.students && teacher.students.length > 0) {
+    // Delete all students (their pre-delete hooks will handle resource requests)
+    await Student.deleteMany({ _id: { $in: teacher.students } });
+  }
+  next();
+});
+
+teacherSchema.pre("findByIdAndDelete", async function(next) {
+  const teacher = await this.model.findById(this.getFilter()._id);
+  if (teacher && teacher.students && teacher.students.length > 0) {
+    // Delete all students (their pre-delete hooks will handle resource requests)
+    await Student.deleteMany({ _id: { $in: teacher.students } });
+  }
+  next();
 });
 
 module.exports = mongoose.model("Teacher", teacherSchema);
