9f0fbe6b0c31f1aae7fba1c426b23cf0a6d4d9bb
Ankit Kumar <rajeraankit4@gmail.com>
2026-03-04 12:13:52 +0530

    sending undertaking pdf attachement in email to student

--- server/main/controllers/student.controller.js
+++ server/main/controllers/student.controller.js
@@ -154,11 +154,19 @@ exports.submitResourceRequest = async (req, res) => {
     if (savedRequest) {
       await addResourceRequestToStudent(studentId, savedRequest._id);
 
-      // Send email to student after successful request
+      // Send email to student after successful request (with undertaking PDF)
       sendResourceRequestSubmittedEmail(
         student.email,
         student.name,
-        savedRequest.title
+        savedRequest.title,
+        {
+          name: student.name,
+          rollNo: student.rollNo,
+          branch: student.branch,
+          instituteName: student.instituteName,
+          instituteAddress: student.instituteAddress
+        },
+        savedRequest.purpose
       ).catch((err) => {
         console.error('Error sending resource request email:', err);
       });
