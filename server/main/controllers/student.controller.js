911e8c9f041a040e484ce4721c8aba4c8e8d5b0d
Ankit Kumar <rajeraankit4@gmail.com>
2026-03-23 00:06:25 +0530

    minor db query mix in getRequestAllotmentTime

--- server/main/controllers/student.controller.js
+++ server/main/controllers/student.controller.js
@@ -279,7 +279,10 @@ exports.getRequestAllotmentTime = async (req, res) => {
     }
 
     // Find the allotment for this request
-    const allotment = await MachineAllotment.findOne({ resourceRequestId: requestId });
+    const allotment = await MachineAllotment.findOne(
+      { resourceRequestId: requestId },
+      { startTime: 1, endTime: 1, _id: 0 }
+    ).lean();
     if (!allotment) {
       return res.status(404).json({ error: "No allotment found for this request" });
     }
