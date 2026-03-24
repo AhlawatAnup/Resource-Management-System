eec5d1aa855fcd7cb4c7e33a23ff43184db068e4
Ankit Kumar <rajeraankit4@gmail.com>
2026-03-24 18:38:44 +0530

    fixed minor logout issue for some pages due to import issues

--- public/dashboard/admin/js/machines/admin-machines.main.js
+++ public/dashboard/admin/js/machines/admin-machines.main.js
@@ -2,6 +2,7 @@
 import * as ui from './admin-machines.ui.js';
 import * as service from './admin-machines.service.js';
 import * as utils from './admin-machines.utils.js';
+import { handleLogout } from '../../../common/js/commons.js';  //for html logout
 import { 
   handleImport, 
   handleDelete, 
