ccda05a880565ab7f03a0b5db1b4ab10c03ecb2e
Ankit Kumar <rajeraankit4@gmail.com>
2026-03-27 19:03:50 +0530

    fetched expiryAllotmentsSchedule from .env

--- server/main/main.server.js
+++ server/main/main.server.js
@@ -143,7 +143,8 @@ schedule.scheduleJob(expiryNotifySchedule, async () => {
 });
 
 // Schedule job to run every day at 00:00
-schedule.scheduleJob("*/1 * * * *", async () => {
+const expiryAllotmentsSchedule = process.env.expiryAllotmentsSchedule || "0 0 * * *";
+schedule.scheduleJob(expiryAllotmentsSchedule, async () => {
   console.log(`Expired allotments scheduler started...`);
   await markExpiredAllotmentsDeleted();
 });
