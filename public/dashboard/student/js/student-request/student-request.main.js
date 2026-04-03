f3ffa1b45e30f26c769462cd8f3d6ef4c8ae8433
Ankit Kumar <rajeraankit4@gmail.com>
2026-04-03 18:44:46 +0530

    added dark mode on all pages of students (except about us)

--- public/dashboard/student/js/student-request/student-request.main.js
+++ public/dashboard/student/js/student-request/student-request.main.js
@@ -1,8 +1,10 @@
 // main.js
 
 import { handleLoadRequestResources } from './student-request.handler.js';
+import { setupDarkMode } from '../../../common/js/darkmode/darkmode.js';
 
 // App initialization
 document.addEventListener('DOMContentLoaded', () => {
+    setupDarkMode();
     handleLoadRequestResources();
 });
--- public/dashboard/student/js/view-requests/student-view-request.main.js
+++ public/dashboard/student/js/view-requests/student-view-request.main.js
@@ -1,7 +1,9 @@
 // main.js
 
 import { handleLoadViewRequests } from './student-view-request.handler.js';
+import { setupDarkMode } from '../../../common/js/darkmode/darkmode.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
+    setupDarkMode();
     await handleLoadViewRequests();
 });
