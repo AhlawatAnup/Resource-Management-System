2aed2533b883ce09db85dc3a3bdc0cbb80ea148f
Ankit Kumar <rajeraankit4@gmail.com>
2026-04-04 19:47:53 +0530

    updated getAllResourceRequests to return startTime and endTime

--- server/main/utils/common.utils.js
+++ server/main/utils/common.utils.js
@@ -392,6 +392,19 @@ const makeMachineHistory = async (studentId) => {
   }
 };
 
+async function getAllotmentMap(requestIds) {
+  const allotments = await MachineAllotment.find(
+    { resourceRequestId: { $in: requestIds } },
+    { resourceRequestId: 1, startTime: 1, endTime: 1 }
+  ).setOptions({ includeInactive: true });
+
+  const allotmentMap = {};
+  for (const a of allotments) {
+    allotmentMap[a.resourceRequestId.toString()] = a;
+  }
+  return allotmentMap;
+}
+
 module.exports = {
   isValidDuration,
   fetchMachineById,
@@ -403,4 +416,5 @@ module.exports = {
   deleteTeacher,
   unverifyStudent,
   makeMachineHistory,
+  getAllotmentMap,
 };
