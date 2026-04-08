2bbe6e0be836ff6cca9ad1024cf3cea1fcbfc149
Ankit Kumar <rajeraankit4@gmail.com>
2026-04-08 23:49:57 +0530

    updated machine stats model

--- server/main/database/machineStatsModel.js
+++ server/main/database/machineStatsModel.js
@@ -2,22 +2,24 @@ const mongoose = require('mongoose');
 
 const MachineStatSchema = new mongoose.Schema({
   timestamp: { type: Date, required: true },
-  MIGID: { type: String, required: true },  
-  user: { type: String, required: true },
-  parentMachine: { type: String, required: true },
-  cpuPerc: Number,
-  memUseMiB: Number,
-  memTotalMiB: Number,
-  gpuVramMiB: Number,
-  gpuTotalMiB: Number,
+  
+  metadata: { MIGID: { type: String, required: true }},
+
+  cpuPerc: { type: Number },
+  memUseMiB: { type: Number },
+  memTotalMiB: { type: Number },
+  gpuVramMiB: { type: Number },
+  gpuTotalMiB: { type: Number }
 }, {
   timeseries: {
     timeField: 'timestamp',
-    metaField: 'MIGID',
+    metaField: 'metadata',
     granularity: 'minutes'
   },
   autoCreate: true
 });
+
+MachineStatSchema.index({ "metadata.MIGID": 1, timestamp: -1 });
 
 function getMachineStatModel(statsConnection) {
   return statsConnection.model('MachineStat', MachineStatSchema);
